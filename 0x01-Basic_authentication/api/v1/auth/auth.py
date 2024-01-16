2f64340b440546e0a1294c0bddb3cba8b83dcf87
root <root@DESKTOP-JKHB9QO>
2024-01-16 15:18:13 +0000

    fixed indentation error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module of API authentication
 """
-
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
@@ -11,7 +11,7 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' Public auth method
         '''
-    return False
+        return False
 
     def authorization_header(self, request=None) -> str:
         '''Authorization header method
