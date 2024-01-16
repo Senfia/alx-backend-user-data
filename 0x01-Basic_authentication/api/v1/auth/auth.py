1a81545ce6fa447b2c798e99c2fc1f10c9ced6bf
root <root@DESKTOP-JKHB9QO>
2024-01-16 15:32:13 +0000

    add slash handler

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' Public auth method
         '''
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        # Ensure that paths in excluded_paths are slash-tolerant
+        excluded_paths = [p.rstrip('/') for p in excluded_paths]
+
+        # Check if the path is in excluded_paths
+        return path.rstrip('/') not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         '''Authorization header method
